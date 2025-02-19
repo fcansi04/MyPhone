cf7b9c3c1900ed4b4927f24c16b40f3bf9e6a18a
fcansi04 <ferhatcansi.04@outlook.com>
2025-02-20 01:54:03 +0300

    a syntax revised

--- app/deleteProducts/page.jsx
+++ app/deleteProducts/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import Filter from "/components/Filter";
+import Filter from "@/components/Filter";
 import { useState, useEffect } from "react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
